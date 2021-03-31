2b4cae6d719899c7e9ac2f2f4dc4d81f57899a1e
Alfendo Rizky <80095807+alfendors@users.noreply.github.com>
2021-03-31 23:16:52 +0700

    Add files via upload

--- Pertemuan 4/src/pratikum4/Main.java
+++ Pertemuan 4/src/pratikum4/Main.java
@@ -1,31 +1,25 @@
-package praktikum4.CPU;
+package praktikum4.MotherBoard;
 
-class CPU {
-    double price;
-    // nested class
-    class Processor{
-        // members of nested class
-        double cores;
-        String manufacturer;
-        double getCache(){
-            return 4.3;
+import java.util.Scanner;
+
+class MotherBoard {
+    // static nested class
+    static class USB{
+        int getTotalPorts(int usb2, int usb3){
+            return usb2 + usb3;
         }
-    }
-    // nested protected class
-    protected class RAM{
-        // members of protected nested class
-        double memory;
-        String manufacturer;
-        double getClockSpeed(){
-            return 5.5;        }
     }
 }
 public class Main {
     public static void main(String[] args) {
-        CPU cpu = new CPU();
-        CPU.Processor proc = cpu.new Processor();
-        CPU.RAM ram = cpu.new RAM();
-        System.out.println("Prosessor Cache" + proc.getCache());
-        System.out.println("Ram Clock Speen" + ram.getClockSpeed());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukan jumlah usb2 = ");
+        int usb2 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Masukan jumlah usb3 = ");
+        int usb3 = Integer.parseInt(scanner.nextLine());
+        MotherBoard.USB usb = new MotherBoard.USB();
+        int totalUsb = usb.getTotalPorts(usb2, usb3);
+        System.out.println("Total Ports = " + totalUsb);
     }
 }
+
